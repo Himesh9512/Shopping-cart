53dfcc491b19258e09686d994dbe94585e59288f
Himesh9512 <himeshjangid0000@gmail.com>
2023-03-18 12:15:22 +0530

    Add data and tailwindcss to project

---
+++ src/utils/data/data.js
@@ -0,0 +1,237 @@
+import Img1 from "../../assets/images/desertx.png";
+import Img2 from "../../assets/images/panigale-v2.png";
+import Img3 from "../../assets/images/panigale-v2-bayliss.png";
+import Img4 from "../../assets/images/panigale-v4.png";
+import Img5 from "../../assets/images/panigale-v4s.png";
+import Img6 from "../../assets/images/panigale-v4r.png";
+import Img7 from "../../assets/images/panigale-v4-sp2.png";
+import Img8 from "../../assets/images/streetfighter-v2.png";
+import Img9 from "../..//assetsimages/streetfighter-v4.png";
+import Img10 from "../../assets/images/streetfighter-v4s.png";
+import Img11 from "../../assets/images/streetfighter-v4-sp2.png";
+import Img12 from "../../assets/images/superleggera.png";
+import Img13 from "../../assets/images/multistrada-v2.png";
+import Img14 from "../../assets/images/multistrada-v2s.png";
+import Img15 from "../../assets/images/multistrada-v4.png";
+import Img16 from "../../assets/images/multistrada-v4-rally.png";
+import Img17 from "../../assets/images/supersport-950.png";
+import Img18 from "../../assets/images/supersport-950-s.png";
+
+export const data = [
+	{
+		id: 1,
+		name: "desertx",
+		power: 110,
+		displacment: 937,
+		torque: 92,
+		weight: 202,
+		description:
+			"Exploration. Fun. Performance. And a great desire to take and leave towards the horizon. The identity of this bike leaves no room for doubt: just looking at it, it brings you into the atmosphere of Dakar racing.",
+		image: Img1,
+		price: 999,
+	},
+	{
+		id: 2,
+		name: "panigale v2",
+		power: 155,
+		displacment: 955,
+		torque: 104,
+		weight: 176,
+		description:
+			"The twin-cylinder Panigale has been given a thorough makeover. Renamed the Panigale V2, it takes a totally new design, it features sophisticated electronics based on a 6-axis inertial platform and, thanks to a more comfortable rider seat and the suspension set-up, offers better yet safer sports performance while making road riding more enjoyable and user-friendly than ever.",
+		image: Img2,
+		price: 999,
+	},
+	{
+		id: 3,
+		name: "panigale v2 bayliss",
+		power: 155,
+		displacment: 955,
+		torque: 104,
+		weight: 174.5,
+		description:
+			"The Panigale V2 Bayliss 1st Championship 20th Anniversary celebrates one of the most successful riders of all time and the symbol of an entire community: Troy Bayliss. A special motorcycle, manufactured in a numbered series, the pure expression of Ducati sportsmanship and a tribute to a timeless passion.",
+		image: Img3,
+		price: 999,
+	},
+	{
+		id: 4,
+		name: "panigale v4",
+		power: 215.5,
+		displacment: 1103,
+		torque: 123.6,
+		weight: 175,
+		description:
+			"The Panigale V4 MY23 represents the last step in the characteristic path of Borgo Panigale's sports bikes. As is the case with motorcycles engaged in MotoGP and WorldSBK, the Panigale V4 is updated and improved year after year, and the 2022 version represents the most significant evolution since its launch year. For MY2023, a series of electronic improvements have been introduced that make the bike even easier and more intuitive for riders of all levels.",
+		image: Img4,
+		price: 999,
+	},
+	{
+		id: 5,
+		name: "panigale v4 s",
+		power: 215.5,
+		displacment: 1103,
+		torque: 123.6,
+		weight: 174,
+		description:
+			"The Panigale V4 MY23 represents the last step in the characteristic path of Borgo Panigale's sports bikes. As is the case with motorcycles engaged in MotoGP and WorldSBK, the Panigale V4 is updated and improved year after year, and the 2022 version represents the most significant evolution since its launch year. For MY2023, a series of electronic improvements have been introduced that make the bike even easier and more intuitive for riders of all levels.",
+		image: Img5,
+		price: 999,
+	},
+	{
+		id: 6,
+		name: "panigale v4 r",
+		power: 237,
+		displacment: 998,
+		torque: 111.3,
+		weight: 172,
+		description:
+			"Ducati presents the new Panigale V4 R: the bike that adopts technical solutions previously reserved for MotoGP and WorldSBK Championship.",
+		image: Img6,
+		price: 999,
+	},
+	{
+		id: 7,
+		name: "panigale v4 sp2",
+		power: 215.5,
+		displacment: 1103,
+		torque: 123.6,
+		weight: 173,
+		description:
+			"The Panigale V4 SP2 is the top-of-the-range model for Ducati super sports bikes: a special, exciting numbered-version for track riding, even more intuitive and less strenuous for non-professional riders.",
+		image: Img7,
+		price: 999,
+	},
+	{
+		id: 8,
+		name: "streetfighter v2",
+		power: 153,
+		displacment: 955,
+		torque: 101.4,
+		weight: 178,
+		description:
+			"The new Streetfighter V2 is a sports naked with a one-of-a-kind character. It accommodates the needs of those looking for a bike that can combine the sporty DNA of the Panigale V2 with both the attitude and style of the much-admired Streetfighter. The result is an intuitive and easy-to-handle vehicle, with the right dose of power to ensure feisty performance and maximum riding fun.",
+		image: Img8,
+		price: 999,
+	},
+	{
+		id: 9,
+		name: "streetfighter v4",
+		power: 208,
+		displacment: 1103,
+		torque: 123,
+		weight: 180,
+		description:
+			"The Streetfighter V4 was an instant success and set new performance benchmarks in the super sports naked segment. Universally appreciated for its aggressive and minimalist aesthetic, characterized by a front inspired by the Joker of the comics, the Streetfighter V4 immediately entered the hearts of enthusiasts.",
+		image: Img9,
+		price: 999,
+	},
+	{
+		id: 10,
+		name: "streetfighter v4 s",
+		power: 208,
+		displacment: 1103,
+		torque: 123,
+		weight: 178,
+		description:
+			"The Streetfighter V4 was an instant success and set new performance benchmarks in the super sports naked segment. Universally appreciated for its aggressive and minimalist aesthetic, characterized by a front inspired by the Joker of the comics, the Streetfighter V4 immediately entered the hearts of enthusiasts.",
+		image: Img10,
+		price: 999,
+	},
+	{
+		id: 11,
+		name: "streetfighter v4 sp2",
+		power: 208,
+		displacment: 1103,
+		torque: 123,
+		weight: 177,
+		description:
+			"The top-of-the-range model in the Streetfighter family is the new Streetfighter V4 SP2 in a numbered version. A bike ready to take to the track, thanks to exclusive technical equipment that combines the Fight Formula with SP specifications, making it even more effective in sporty riding and particularly on the circuit.",
+		image: Img11,
+		price: 999,
+	},
+	{
+		id: 12,
+		name: "superleggera",
+		power: 224,
+		displacment: 998,
+		torque: 116,
+		weight: 159,
+		description:
+			"The epitome of engineering excellence, cutting-edge technology and aerodynamic design. The new Superleggera V4 represents the dream of designing the only motorcycle in the world approved for road use with carbon fibre frame, swinging arm and rims with an unbeatable power/weight ratio of 1.54 hp/kg.",
+		image: Img12,
+		price: 999,
+	},
+	{
+		id: 13,
+		name: "multistrada v2",
+		power: 113,
+		displacment: 937,
+		torque: 96,
+		weight: 199,
+		description:
+			"The Ducati Multistrada family continues its evolution with the Multistrada V2: the ideal bike to fully enjoy every route, versatile in everyday use and in the city, always with the sportiness and unmistakable style that characterize every Ducati.",
+		image: Img13,
+		price: 999,
+	},
+	{
+		id: 14,
+		name: "multistrada v2 s",
+		power: 113,
+		displacment: 937,
+		torque: 96,
+		weight: 202,
+		description:
+			"The Ducati Multistrada family continues its evolution with the Multistrada V2: the ideal bike to fully enjoy every route, versatile in everyday use and in the city, always with the sportiness and unmistakable style that characterize every Ducati.",
+		image: Img14,
+		price: 999,
+	},
+	{
+		id: 15,
+		name: "multistrada v4",
+		power: 170,
+		displacment: 1158,
+		torque: 125,
+		weight: 215,
+		description:
+			"Get ready to discover the fourth Multistrada generation. Sportier and more touring, stronger in off-road use and easier in urban action. Lightweight and compact, with the new 170 HP V4 Granturismo engine for a sporty and exciting ride on mixed terrain.",
+		image: Img15,
+		price: 999,
+	},
+	{
+		id: 16,
+		name: "multistrada v4 rally",
+		power: 170,
+		displacment: 1158,
+		torque: 121,
+		weight: 227,
+		description:
+			"The new Multistrada V4 Rally is the ideal travel companion in all conditions, as easy and intuitive in the urban jungle as it is on the most daring off-road routes. High-performance, robust and reliable thanks to the V4 Granturismo and extended service intervals, it will carry you wherever your adventurous heart desires.",
+		image: Img16,
+		price: 999,
+	},
+	{
+		id: 17,
+		name: "supersport 950",
+		power: 110,
+		displacment: 937,
+		torque: 93,
+		weight: 184,
+		description:
+			"The design of the SuperSport 950 evolves thanks to the fairings designed by the Ducati Style Centre which give the bike more sportiness, dynamism, fluidity in shapes and a strong and decisive resemblance to the Panigale V4, together with the full-LED headlamp equipped with DRL.",
+		image: Img17,
+		price: 999,
+	},
+	{
+		id: 18,
+		name: "supersport 950 s",
+		power: 110,
+		displacment: 937,
+		torque: 93,
+		weight: 184,
+		description:
+			"The design of the SuperSport 950 evolves thanks to the fairings designed by the Ducati Style Centre which give the bike more sportiness, dynamism, fluidity in shapes and a strong and decisive resemblance to the Panigale V4, together with the full-LED headlamp equipped with DRL.",
+		image: Img18,
+		price: 999,
+	},
+];
